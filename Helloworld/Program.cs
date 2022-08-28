9d8bf5219abe61e45994762006252471982d707a
danielAdama <adamadaniel321.com>
2022-08-28 23:45:45 +0100

    First Tutorials

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -11,16 +11,20 @@
 
 using System;
 
-public class MyFirstApp
+namespace Helloworld
+{
+    public class MyFirstApp
 {
     public static void Main(string[] args)
     {
         string username = "Daniel";
         int number = 2;
         float price = 3.99f;
+        
 
         Console.WriteLine("Hello "+username);
         Console.WriteLine("The total price is: "+(number*price));
 
     }
+}
 }
