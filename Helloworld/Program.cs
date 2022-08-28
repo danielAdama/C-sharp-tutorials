60eb2cb729dd12674a91eb7ec1be13ab1e225404
danielAdama <adamadaniel321.com>
2022-08-28 15:05:33 +0100

    Initial Commit

---
+++ Helloworld/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello World!");
+//     }
+// }
+
+using System;
+
+public class MyFirstApp
+{
+    public static void Main(string[] args)
+    {
+        string username = "Daniel";
+        int number = 2;
+        float price = 3.99f;
+
+        Console.WriteLine("Hello "+username);
+        Console.WriteLine("The total price is: "+(number*price));
+
+    }
+}
