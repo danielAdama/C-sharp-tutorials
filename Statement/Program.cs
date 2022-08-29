a0654121c25c3aac29a754e19817573e6bc96827
danielAdama <adamadaniel321.com>
2022-08-29 20:51:50 +0100

    Initial Commit

---
+++ Statement/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Statement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            // Conditional Statements
+            if (temp < 10)
+            {
+                Console.WriteLine("Take the Coat");
+            }
+            else if (temp == 10)
+            {
+                Console.WriteLine("it's 10 degree");
+            }
+            else
+            {
+                Console.WriteLine("It's cozy and cold");
+            }
+
+
+        }
+    }
+}
