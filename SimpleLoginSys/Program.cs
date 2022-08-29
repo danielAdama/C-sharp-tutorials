57cc1db5f9d77151a00a4a59e67166f17e57ad00
danielAdama <adamadaniel321.com>
2022-08-29 20:52:34 +0100

    Initial Commit

---
+++ SimpleLoginSys/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleLoginSys
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // A Login system that signifies, if a user is registered and have a userName print their username,
+            // and if that userName is admin, print admin
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your Username: ");
+            userName = Console.ReadLine();
+
+            if (isRegistered)
+            {
+                Console.WriteLine("Hi there, registered User");
+
+                if (userName != "")
+                {
+                    Console.WriteLine($"Hey {userName}");
+
+                    if (userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hello Admin!");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
