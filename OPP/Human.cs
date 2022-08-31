49b502656a6396cb92ffb027d28668142dcc01a9
danielAdama <adamadaniel321.com>
2022-08-31 22:50:38 +0100

    Improved Script 2

--- OPP/Human.cs
+++ OPP/Human.cs
@@ -17,7 +17,23 @@ namespace OPP
         private string eye_color;
         private int age;
 
-        // Constructors
+        // Default Constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Object created");
+        }
+
+
+        // // Parameterized Constructors
+        // public Human(string firstName, string last_name, string eye_color, int age)
+        // {
+        //     first_name = firstName;
+        //     this.last_name = last_name; // this it used to get a value from a previous variable.
+        //     this.eye_color = eye_color;
+        //     this.age = age;
+
+        // }
+
         public Human(string firstName, string last_name, string eye_color, int age)
         {
             first_name = firstName;
@@ -27,10 +43,50 @@ namespace OPP
 
         }
 
+        public Human(string firstName, string last_name)
+        {
+            this.first_name = first_name;
+
+        }
+        
+        public Human(string firstName, string last_name)
+        {
+            first_name = firstName;
+            this.last_name = last_name;
+
+        }
+
+        public Human(string eye_color, int age)
+        {
+            this.eye_color = eye_color;
+            this.age = age;
+
+        }
+
         // A Method/Function of class Human
         public void introduceMyself()
         {
-            Console.WriteLine($"Hi, I am {first_name} {last_name} with {eye_color} eyes of age {age}");
+            if (age != 0 && last_name != null && eye_color != null && first_name != null)
+            {
+                Console.WriteLine($"Hi, I am {first_name} {last_name} with {eye_color} eyes of age {age}");
+            }
+            else if (age != 0 && last_name != null && first_name != null)
+            {
+                Console.WriteLine($"Hi, I am {first_name} {last_name} of age {age}");
+            }
+            else if (last_name != null && first_name != null)
+            {
+                Console.WriteLine($"Hi, I am {first_name} {last_name}");
+            }
+            else if (first_name != null)
+            {
+                Console.WriteLine($"Hi, I am {first_name}");
+            }
+            else
+            {
+                Console.WriteLine($"Finished");
+            }
+            
         }
 
 
